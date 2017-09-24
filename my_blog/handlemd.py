66485611ac3f46aa1b52d585c8536376fa9cfbdc
Maxwell <2894235159@qq.com>
2017-09-25 07:59:39 +0800

    2017.9.25

--- my_blog/handlemd.py
+++ my_blog/handlemd.py
@@ -3,6 +3,7 @@ markdown transformer by maxwell yang blog
 '''
 import markdown
 import os
+import sys
 from webtext import complete_store 
 
 if __name__=='__main__':
@@ -14,9 +15,14 @@ if __name__=='__main__':
             mdfile=open(i,'r',encoding='utf-8')
             mdfiledata=mdfile.read()
             md=markdown.markdown(mdfiledata)
-            complete_store(md,title=name)
+            if sys.argv[1]=='change':
+                complete_store(md,title=name,change=1,id=sys.argv[2])
+                break   
+            else:
+                complete_store(md,title=name)
             mdfile.close()
             index+=1
+            os.remove(i)                                              #处理完删除文件
             print('已处理完 %s,总共处理md文件 %d个'%(i,index))
     if index==0:
         print('no markdown file to handle!')
--- my_blog/webtext.py
+++ my_blog/webtext.py
@@ -13,59 +13,81 @@ DATE=time.strftime('%Y-%m-%d',time.localtime(time.time()))
 PORT = 8000
 timec=time.time()
 def handle_image(path,index):
-    PATH=os.path.join(os.getcwd(),'image')
+    PATH=os.path.join('githubpage','images')                                      #处理图片文件
     if os.path.isfile(path):
         newname=str(int(timec))+str(index)+'.jpg'
-        shutil.copyfile(path,os.path.join(PATH,newname))                                
+        shutil.copyfile(path,os.path.join(PATH,newname))
+        os.renames(path,os.path.join('images',newname))                           #处理完成后将本地图片文件名与page文件名统一方便更改                                
         return ['s',newname]                           
     else:
         return ['n']                               
 
 
-def handlehtml(html,title=""):
+def handlehtml(html,title="",change=0,id=0):                                             #使用*argc 和**argv 可以为传参带来很多方便
     chtml=BeautifulSoup(html,'html.parser')
     img=chtml.find_all('img')                
-    for i in range(len(img)):                                                    #这里可能会有bug
+    for i in range(len(img)):                                                      #这里可能会有bug
         re=handle_image(img[i]['src'],i)
         #print("handle image %d complete"%(i))                                     #不能加载服务器文件夹之外的文件 预计解决方案直接打开html文件 问题无法返回200关闭页面
         if re[0]=='s':
-            img[i]['src']=os.path.join('image',re[1])        
+            img[i]['src']=os.path.join('images',re[1])        
         elif re[0]=='n':
             pass
             '''mistake_store''' 
         else:
             pass
-    complete_store(str(chtml),title=title)
-
-def complete_store(html,title=""):
-    if title=="":
-        for i in sys.argv[2:]:
-            title+=i
-            title+=" "
-    #outcome='''{"postintro":{"id":%d,"title":"%s","date":"%s"},"post":{"id":%d,"content":'%s'}};'''%(timec,title,DATE,timec,html)
-    #boutcome=outcome.encode('utf-8')
-    outcome={"postintro":{"id":timec,"title":title,"date":DATE},"post":{"id":timec,"content":html}}
-    outcome=json.dumps(outcome)+";"
-    boutcome=outcome.encode('utf-8')
-    with open(os.path.join('outcome','rawpostdata.js'),'ab+') as f:  
-        f.write(boutcome)
-    with open(os.path.join('outcome','rawpostdata.js'),'rb+') as f:
-        data=f.read().decode().split(';')
-        data.pop(-1)
-        with open(os.path.join('outcome','postdata.js'),'wb+') as f:
-            outcome="var data=function(){ var data="+str(data)+";return data;}"
+    try:
+        complete_store(str(chtml),title=title,change=change,id=id)
+    except:
+         complete_store(str(chtml),title=title)
+def complete_store(html,title="",change=0,id=0):
+    if change==1:
+        with open(os.path.join('outcome','rawpostdata.js'),'wb+') as f:
+            data=f.read().decode().split(';')
+            for i in data:
+                ji=json.loads(i)
+                if ji['postintro']['id']==argv['id']:
+                    ji['post']['content']==html
+            outcome=data.join(';')+';'
             boutcome=outcome.encode('utf-8')
             f.write(boutcome)
+    else:
+        if title=="":
+            for i in sys.argv[2:]:
+                title+=i
+                title+=" "
+        outcome={"postintro":{"id":timec,"title":title,"date":DATE},"post":{"id":timec,"content":html}}
+        outcome=json.dumps(outcome)+";"
+        boutcome=outcome.encode('utf-8')
+        with open(os.path.join('outcome','rawpostdata.js'),'ab+') as f:  
+            f.write(boutcome)
+        with open(os.path.join('outcome','rawpostdata.js'),'rb+') as f:
+            data=f.read().decode().split(';')
+            data.pop(-1)
+            with open(os.path.join('outcome','postdata.js'),'wb+') as f:
+                outcome="var data=function(){ var data="+str(data)+";return data;}"
+                boutcome=outcome.encode('utf-8')
+                f.write(boutcome)
+        shutil.copyfile(os.path.join('outcome','postdata.js'),os.path.join('githubpage','javascripts','postdata.js'))
 class handle(hs.SimpleHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
     def do_POST(self):
         self.send_response(200)
         self.send_header('content-type','text/plain')
         self.end_headers()        
         data=self.rfile.read(int(self.headers['content-length']))
         data.decode()
-        handlehtml(data)
+        try:
+            changedata=json.loads(data)
+            handlehtml(changedata['post']['content'],id=changedata['post']['id'],change=1)
+            with open(os.path.join('outcome','temppostdata.js'),'w+') as f:
+                f.truncate()
+        except:
+            handlehtml(data)
         raise KeyboardInterrupt
-        
 
 if __name__=='__main__':
     with socketserver.TCPServer(('localhost',PORT), handle) as httpd:                   
